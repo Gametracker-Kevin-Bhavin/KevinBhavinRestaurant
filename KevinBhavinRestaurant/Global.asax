51418e378217e578be448e81fcbc4a773ebe5905
Jan Kevin Munar <jankevinmunar@gmail.com>
2016-11-14 16:12:43 -0500

    Add project files.

---
+++ KevinBhavinRestaurant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KevinBhavinRestaurant.MvcApplication" Language="C#" %>
---
+++ KevinBhavinRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KevinBhavinRestaurant.Startup))]
+namespace KevinBhavinRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
